1d67884f8193939d4db4aacccf0c5c0c5da69fa2
jsKwak <max6383@naver.com>
2022-04-11 03:20:32 +0900

    개발환경 세팅

---
+++ src/test/java/jpabook/jpabookstore/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package jpabook.jpabookstore;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
+        //given
+        Member member = new Member();
+        member.setUserName("테스트1");
+
+        //when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        //then
+        Assertions.assertThat(saveId).isEqualTo(findMember.getId());
+        Assertions.assertThat(member.getUserName()).isEqualTo(findMember.getUserName());
+
+
+    }
+
+
+}
