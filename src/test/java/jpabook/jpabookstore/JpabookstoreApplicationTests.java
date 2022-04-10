b84e9fe7dcf4841404973af274624ca5535b6df7
jsKwak <max6383@naver.com>
2022-04-11 02:34:13 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpabookstore/JpabookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpabookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpabookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
