de77b1211794afca868f6b7a16450159706f59c0
jsKwak <max6383@naver.com>
2022-04-13 02:50:34 +0900

    Member 관련 비즈니스 로직 개발

--- src/main/java/jpabook/jpabookstore/domain/Category.java
+++ src/main/java/jpabook/jpabookstore/domain/Category.java
@@ -1,9 +1,40 @@
 package jpabook.jpabookstore.domain;
 
-import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
+@Getter @Setter
 public class Category {
 
+    @Id @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+               joinColumns = @JoinColumn(name = "category_id"),
+               inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    private List<Item> items = new ArrayList<>();
+    @OneToMany(mappedBy = "parent")
+    private List<Category> childList = new ArrayList<>();
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    public void addChildCategory(Category child){
+        this.childList.add(child);
+        child.setParent(this);
+    }
 
 }
--- src/main/java/jpabook/jpabookstore/domain/Orders.java
+++ src/main/java/jpabook/jpabookstore/domain/Orders.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -23,11 +25,31 @@ public class Orders {
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    List<OrderItem> orderItems = new ArrayList<>();
+
+
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
     private LocalDateTime orderDate;
 
 
+    // == 연관관계 편의 메서드 == //
+
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
