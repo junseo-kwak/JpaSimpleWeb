0030241e2c16474d539b31e2e2520fbf661b3345
jskwak <max6383@naver.com>
2022-04-13 18:04:46 +0900

    commit

--- src/main/java/jpabook/jpabookstore/domain/Item.java
+++ src/main/java/jpabook/jpabookstore/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpabookstore.domain;
 
+import jpabook.jpabookstore.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,10 +15,23 @@ public abstract class Item {
     @Id @GeneratedValue
     @Column(name = "item_id")
     private Long id;
-
     private String name;
     private int price;
     private int stockQuantity;
+
+
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 
 }
---
+++ src/main/java/jpabook/jpabookstore/repository/ItemRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpabookstore.repository;
+
+import jpabook.jpabookstore.domain.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item){
+
+        if(item.getId() != null){
+            em.merge(item);
+        }else{
+            em.persist(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class,id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i",Item.class)
+                .getResultList();
+    }
+
+
+
+}
